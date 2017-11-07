baf4acd4dfdf45a159320162286f8f1bc6bfb8a6
Ian Perkins <ianperkins57@gmail.com>
2017-11-06 19:24:27 -0600

    deleting old hw2 that didnt have anything usable

--- Shakespeare - HW3/HW3.cpp
+++ Shakespeare - HW3/HW3.cpp
@@ -1,15 +1,27 @@
 #include <iostream>
+#include <fstream>
 
 using namespace std;
 
+void preprocess(string inputFile) {
+	ifstream iStream(inputFile.c_str());
+	string line;
+
+	while(iStream.is_open() && iStream >> line) {
+
+	}
+}
+
 int main(int argc, char * argv[]) {
-	if(argc != 2) {
-		cout << "USAGE: ./a.out sentence" << endl;
+	if(argc != 3) {
+		cout << "USAGE: ./a.out Shakespeare.txt sentence" << endl;
 		return 1;
 	}
 
-	string sentence = argv[1];
-	
+	string sentence = string(argv[1]);
+	string inputFile = string(argv[2]);
+
+	preprocess(inputFile);
 
 	return 0;
 }
