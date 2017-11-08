22a5c2a8030df656d6f1f9e6c8f606b917ee75cd
Ian Perkins <ianperkins57@gmail.com>
2017-11-07 22:02:15 -0600

    normalization and logProb()

--- Shakespeare - HW3/HW3.cpp
+++ Shakespeare - HW3/HW3.cpp
@@ -6,7 +6,7 @@
 #include <math.h>
 
 using namespace std;
-#define BigramList list<tuple<string, string, int, float> >
+#define BigramList list<tuple<string, string, int, long double> >
 #define BigramMap unordered_map<string, BigramList>
 #define UnigramMap unordered_map<string, int>
 
@@ -18,7 +18,7 @@ void mapInit(string inputFile, BigramMap& bigramMap, UnigramMap& unigramMap) {
 	string firstWord, secondWord;
 	BigramMap::iterator bigramIter;
 	BigramList::iterator listIter;
-	tuple<string, string, int, float> tup; // tuple<firstWord, secondWord, frequency/count, probability>
+	tuple<string, string, int, long double> tup; // tuple<firstWord, secondWord, frequency/count, probability>
 
 	iStream >> firstWord;
 	while(iStream) {
@@ -52,6 +52,36 @@ void mapInit(string inputFile, BigramMap& bigramMap, UnigramMap& unigramMap) {
 	}
 }
 
+void normalizeByUnigram(BigramMap& bm, UnigramMap& um) {
+	for(BigramMap::iterator bIter = bm.begin(); bIter != bm.end(); bIter++) {
+		for(BigramList::iterator lIter = bIter->second.begin(); lIter != bIter->second.end(); lIter++) {
+			get<3>(*lIter) = (long double)get<2>(*lIter) / um[bIter->first];
+		}
+	}
+}
+
+void logProbability(BigramMap bm, list<string> words) {
+	BigramMap::iterator bIter;
+	long double answer = 0.0;
+	for(size_t i = 0; i < words.size() - 1; i++) {
+		bIter = bm.find(words.front());
+		words.pop_front();
+		if(bIter != bm.end()) {
+			for(auto lIter = bIter->second.begin(); lIter != bIter->second.end(); lIter++) {
+				if(get<1>(*lIter) == words.front()) {
+					answer += (long double)log10(get<3>(*lIter));
+					break;
+				}
+			}
+		}
+	}
+	cout << "log probability = " << (long double)answer << endl;
+}
+
+void shannonVisualization(BigramMap bm, string fw, string sw) {
+
+}
+
 void printBigramMap(BigramMap bm) {
 	for(BigramMap::const_iterator bigramIter = bm.begin(); bigramIter != bm.end(); bigramIter++) {
 		for(BigramList::const_iterator listIter = bigramIter->second.begin(); listIter != bigramIter->second.end(); listIter++) {
@@ -61,18 +91,29 @@ void printBigramMap(BigramMap bm) {
 }
 
 int main(int argc, char * argv[]) {
-	if(argc <= 3) {
+	if(argc < 4) {
 		cout << "USAGE: ./a.out dir/to/file.txt sentence" << endl;
+		cout << "OR" << endl << "USAGE: ./a.out dir/to/file.txt firstWord secondWord" << endl;
 		return 1;
 	}
 
 	BigramMap bigramMap;
 	UnigramMap unigramMap;
-	string inputFile = string(argv[1]);
-	string sentence = string(argv[2]);
+	string inputFile = string(argv[1]), firstWord, secondWord;
+	list<string> words;
 
 	mapInit(inputFile, bigramMap, unigramMap);
-	printBigramMap(bigramMap);
+	normalizeByUnigram(bigramMap, unigramMap);
+	// printBigramMap(bigramMap);
+
+	if(argc == 4) {
+		firstWord = argv[2];
+		secondWord = argv[3];
+		shannonVisualization(bigramMap, firstWord, secondWord);
+	} else {
+		for(int i = 2; i < argc; i++) { words.push_back(argv[i]); }
+		logProbability(bigramMap, words);
+	}
 
 	return 0;
 }
